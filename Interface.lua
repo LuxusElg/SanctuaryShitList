f5ba3d88b477a40e6022629f02cee95019bf4f39
jorgen <jorgen@ellera.no>
2019-09-07 17:08:17 +0200

    Added childpanel

--- Interface.lua
+++ Interface.lua
@@ -1,3 +1,10 @@
+-- register Interface options page
 local panel = CreateFrame("FRAME")
-panel.name = "Sanctuary Shit List"
+panel.name = "SanctuaryShitList"
 InterfaceOptions_AddCategory(panel)
+
+-- add content
+childpanel = CrateFrame("FRAME", "Shitlists", panel)
+childpanel.name = "Shitlistsss"
+childpanel.parent = panel.name
+InterfaceOptions_AddCategory(childpanel)
